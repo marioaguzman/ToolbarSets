6db6f0d33787c025791018cf1c4f11841ae2f980
Mario Guzman <mario.guzman@mac.com>
2022-07-18 21:07:33 -0700

    Initial Commit

---
+++ ToolbarSets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ToolbarSets
+//
+//  Created by Mario Guzman on 7/18/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
